79956507051c90973bbbbc6b95ebb9d2cac846ea
Stephen Franklin <plamtruepub@gmail.com>
2014-05-10 20:07:35 -0500

    plot1.R reading limited dataset and combining date and time.

---
+++ plot1.R
@@ -0,0 +1,15 @@
+# plot1.R
+# 
+zipfile <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url=zipfile,destfile="household_power_consumption.zip")
+unzip("household_power_consumption.zip", overwrite=T)
+file <- "household_power_consumption.txt"
+cols<-c("Date","Time","Global_active_power","Global_reactive_power",
+        "Voltage","Global_intensity","Sub_metering_1",
+        "Sub_metering_2","Sub_metering_3")
+power <- read.table(file,header=T,sep=";",col.names=cols,
+                    nrows=2880,skip=66636)
+View(power)
+power$datetime<-paste(power$Date,power$Time)
+power$datetime<-strptime(power$datetime,"%d/%m/%Y %H:%M")
+
